c58b67c9c90e96cea53c237a024d92332de87ff9
Prince Tomar <madm47157@gmail.com>
2023-08-25 00:17:32 +0530

    Created basic video youtube template

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import "./App.css";
 import Video from "./components/Video";
 
 function App() {
+  let obj = {
+    title: "Node JS Tutorial",
+    imageURL:
+      "https://www.freecodecamp.org/news/content/images/2021/10/node4.png",
+    views: "10 K",
+    time: "1 year ago",
+    channel: "Javascript Mastery",
+  };
   return (
-    <div>
-      Hello
+    <div className="App">
+      <div> Videos</div>
       <div>
-        <Video
-          title="Node JS Tutorial"
-          imageURL="https://www.freecodecamp.org/news/content/images/2021/10/node4.png"
-        ></Video>
+        <Video {...obj}></Video>
 
         <Video
           title="Flutter Tutorial"
           imageURL="https://www.freecodecamp.org/news/content/images/2022/02/flutter37.png"
+          views="20 K"
+          time="4 months ago"
+          channel="CodeFreeCamp.Org"
         ></Video>
       </div>
     </div>
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,19 +1,22 @@
 import "./Video.css";
 
-function Video({ title, imageURL }) {
+function Video({ title, imageURL, channel = "YouFube", views, time }) {
   //   const Topic = "React JS Tuto  rial";
-  let bg = "dark";
-  let imageDimensionStyle = "imageDim";
+  //   let bg = "dark";
   return (
-    <>
-      <img
-        src={imageURL}
-        alt="Youtube Thumbnail"
-        className={imageDimensionStyle}
-      />
+    <div className="container">
+      <div className="pic">
+        <img src={imageURL} alt="Youtube Thumbnail" />
+      </div>
 
-      <div className={bg}>{[title]}</div>
-    </>
+      <div className="title">{title}</div>
+      <div className="channel">{channel}</div>
+      <div className="views">
+        {views} views
+        <span>.</span>
+        {time}
+      </div>
+    </div>
   );
 }
 
